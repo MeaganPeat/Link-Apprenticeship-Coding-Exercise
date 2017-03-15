d24b1b2fae18c1da5944ba247f902cc266ebfa5e
Meagan <peatm@sheridanc.on.ca>
2017-03-15 14:51:35 -0400

    Set up for second parameter - numPeople

--- costCalculator.js
+++ costCalculator.js
@@ -1,9 +1,12 @@
 // write a function to calculate costs
-function calculateCosts(basePrice){
+function calculateCosts(basePrice, numPeople){
     //markups
     var flatMarkup = 1.05;
+    var numPeopleMarkup = 0.012;
     
     var basePrice = basePrice;
+    var numPeople = numPeople;
+    
     var finalCost = basePrice*flatMarkup;
     return finalCost;
 }
