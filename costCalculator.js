3e99140b6dd84801d09e26d488c1dd17be8d729f
Meagan <peatm@sheridanc.on.ca>
2017-03-15 14:46:04 -0400

    Make the markup a variable (for clarity and for future changes)

--- costCalculator.js
+++ costCalculator.js
@@ -1,6 +1,9 @@
 // write a function to calculate costs
 function calculateCosts(basePrice){
+    //markups
+    var flatMarkup = 1.05;
+    
     var basePrice = basePrice;
-    var finalCost = basePrice*1.05;
+    var finalCost = basePrice*flatMarkup;
     return finalCost;
 }
