d4757a497831560283a2075e5232c2a6f5116e94
Meagan <peatm@sheridanc.on.ca>
2017-03-21 11:24:53 -0400

    Adding tests for all examples

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -2,8 +2,16 @@ var expect = require("chai").expect;
 var tools = require("../tools");
 
 describe("calculateCosts()", function(){
-    it("should calculate the final cost", function(){
+    it("should calculate the final cost for example 1", function(){
         var results = tools.calculateCosts(1299.99, 3, "food");
         expect(results).to.equal(1591.58);
+    });
+    it("should calculate the final cost for example 2", function(){
+        var results = tools.calculateCosts(5432, 1, "drugs");
+        expect(results).to.equal(6199.81);
+    });
+    it("should calculate the final cost for example 3", function(){
+        var results = tools.calculateCosts(12456.95, 4, "books");
+        expect(results).to.equal(13707.63);
     });
 });
