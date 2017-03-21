55736048a28038aad08bead01afcb478eefc7192
Meagan <peatm@sheridanc.on.ca>
2017-03-21 10:31:51 -0400

    Going with modular format! Whoooo (I actually really like using modules)

--- costCalculator.js
+++ costCalculator.js
@@ -1,66 +1,63 @@
-// write a function to calculate costs
-function calculateCosts(basePrice, numPeople, materialType){
-    ////MARKUP VALUES////
-    var flatMarkup = 1.05;
-    var numPeopleMarkup = 0.012;
+var tools = function(tools){
 
-    var materialMarkup;
-    var drugMarkup = 0.075;
-    var foodMarkup = 0.13;
-    var electronicsMarkup = 0.02;
-    
-    ////GET PARAMETERS////
-    var basePrice = basePrice;
-    
-    //for number of people
-    //check if the number of people is provided (as a number)
-    if(numPeople&&numPeople!=NaN){
-        //use that value
-        var numPeople = numPeople;
-    } else {
-        //assume one person
-        var numPeople = 1;
-    }   
-    
-    //for materialType
-    //check if there is a provided material and if it matches
-    if(materialType){
-        var materialType = materialType;
-        var material = materialType.toLocaleLowerCase();
-        if(material == 'drug'||
-           material == 'drugs'||
-           material == 'pharma' || 
-           material == 'pharmaceuticals'){
-            materialMarkup = drugMarkup;
-        } else if(material == 'food'){
-            materialMarkup = foodMarkup;
-        } else if(material == 'electronics'){
-            materialMarkup = electronicsMarkup;
+    // write a function to calculate costs
+    tools.calculateCosts(basePrice, numPeople, materialType){
+        ////MARKUP VALUES////
+        var flatMarkup = 1.05;
+        var numPeopleMarkup = 0.012;
+
+        var materialMarkup;
+        var drugMarkup = 0.075;
+        var foodMarkup = 0.13;
+        var electronicsMarkup = 0.02;
+
+        ////GET PARAMETERS////
+        var basePrice = basePrice;
+
+        //for number of people
+        //check if the number of people is provided (as a number)
+        if(numPeople&&numPeople!=NaN){
+            //use that value
+            var numPeople = numPeople;
         } else {
-            //if its anything else
+            //assume one person
+            var numPeople = 1;
+        }   
+
+        //for materialType
+        //check if there is a provided material and if it matches
+        if(materialType){
+            var materialType = materialType;
+            var material = materialType.toLocaleLowerCase();
+            if(material == 'drug'||
+               material == 'drugs'||
+               material == 'pharma' || 
+               material == 'pharmaceuticals'){
+                materialMarkup = drugMarkup;
+            } else if(material == 'food'){
+                materialMarkup = foodMarkup;
+            } else if(material == 'electronics'){
+                materialMarkup = electronicsMarkup;
+            } else {
+                //if its anything else
+                materialMarkup = 0;
+            }
+        } else {
+            //if no material was provided
             materialMarkup = 0;
         }
-    } else {
-        //if no material was provided
-        materialMarkup = 0;
-    }
-    
-    ////CALCULATE////
-    var flatMarkupCost = basePrice*flatMarkup;
-    var addedMarkup = numPeople*numPeopleMarkup + materialMarkup;
-    var finalMarkup = addedMarkup + 1;
-    
-    var finalCost = flatMarkupCost*finalMarkup;
-    var finalCostRounded = Math.round(finalCost*100)/100;
-    
-    return finalCostRounded;
-}
 
-//Example One
-console.log("Example one costs $"+calculateCosts(1299.992, 3, "food")+'.');
+        ////CALCULATE////
+        var flatMarkupCost = basePrice*flatMarkup;
+        var addedMarkup = numPeople*numPeopleMarkup + materialMarkup;
+        var finalMarkup = addedMarkup + 1;
 
-//Example Two
-console.log("Example two costs $" + calculateCosts(5432, 1, "drugs")+'.');
+        var finalCost = flatMarkupCost*finalMarkup;
+        var finalCostRounded = Math.round(finalCost*100)/100;
 
-//Example Three
-console.log("Example three costs $" + calculateCosts(12456.95, 4, "books")+'.');
+        return finalCostRounded;
+    }   
+    
+    
+    return tools;
+}(tools||{});
--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,8 +1,8 @@
 var expect = require("chai").expect;
-var calculator = require("../costCalculator");
+var tools = require("../tools");
 
 describe("calculateCosts()", function(){
     it("should calculate the final cost", function(){
-        
+        var results = tools.costCalculator();
     });
 });
