5f5328cdcba3b6409baa613ec1589d3ff6397f19
Meagan <peatm@sheridanc.on.ca>
2017-03-21 11:15:27 -0400

    move into a tools module and test that module

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -3,7 +3,7 @@ var tools = require("../tools");
 
 describe("calculateCosts()", function(){
     it("should calculate the final cost", function(){
-        var results = tools.costCalculator(1299.99, 3, "food");
+        var results = tools.calculateCosts(1299.99, 3, "food");
         expect(results).to.equal(1591.58);
     });
 });
